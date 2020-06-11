1194279f97981c468f2a6dd6976303c22340f004
virtual <1185513330@qq.com>
2020-06-11 21:01:47 +0800

    ci build文件升级,增加FileSystem模块
    Signed-off-by: virtual <1185513330@qq.com>

---
+++ framework/FileSystem/src/SharpAbp.FileSystem/FileSystem/SharpAbpFileSystemModule.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp;
+using Volo.Abp.Modularity;
+
+namespace SharpAbp.FileSystem
+{
+    public class SharpAbpFileSystemModule : AbpModule
+    {
+
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+        }
+
+        public override void OnApplicationInitialization(ApplicationInitializationContext context)
+        {
+        }
+
+    }
+}
