218b0a2b340864d8fdc655280fdd033bb044ae62
virtual <1185513330@qq.com>
2020-08-06 20:23:50 +0800

    init test
    Signed-off-by: virtual <1185513330@qq.com>

---
+++ framework/test/SharpAbp.Abp.Spool.Tests/AbpSpoolModuleTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Spool;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.Modularity;
+using Xunit;
+
+namespace SharpAbp.Abp.Spool.Tests
+{
+    public class AbpSpoolModuleTest
+    {
+
+        [Fact]
+        public void InitTest()
+        {
+            var mockServiceConfigurationContext = new Mock<ServiceConfigurationContext>();
+
+            var module = new AbpSpoolModule();
+
+            //module.ConfigureServices(mockServiceConfigurationContext.Object);
+
+            //mockServiceConfigurationContext.Verify(x => x.Services.Add(It.IsAny<ServiceDescriptor>()), Times.Once);
+
+        }
+    }
+}
