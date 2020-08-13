ad31783059128db5c42da0530236ac4db9ee4e47
virtual <1185513330@qq.com>
2020-08-13 21:08:30 +0800

    #1 Create FileIdentity Signed-off-by: virtual <1185513330@qq.com>

--- framework/src/SharpAbp.Abp.FileStoring/SharpAbp/Abp/FileStoring/AbpFileStoringModule.cs
+++ framework/src/SharpAbp.Abp.FileStoring/SharpAbp/Abp/FileStoring/AbpFileStoringModule.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Text;
 using Volo.Abp.Modularity;
+using Volo.Abp.MultiTenancy;
 
 namespace SharpAbp.Abp.FileStoring
 {
+    [DependsOn(typeof(AbpMultiTenancyModule))]
     public class AbpFileStoringModule : AbpModule
     {
 
---
+++ framework/src/SharpAbp.Abp.FileStoring/SharpAbp/Abp/FileStoring/DefaultFileStore.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.MultiTenancy;
+
+namespace SharpAbp.Abp.FileStoring
+{
+    public class DefaultFileStore : IFileStore, ITransientDependency
+    {
+        private readonly ILogger _logger;
+
+        private readonly ICurrentTenant _currentTenant;
+        public DefaultFileStore(ILogger<DefaultFileStore> logger, ICurrentTenant currentTenant)
+        {
+            _logger = logger;
+            _currentTenant = currentTenant;
+        }
+
+        //public async Task<FileIdentity> UploadFileAsync(string filePath)
+        //{
+        //    return default;
+        //}
+    }
+}
