45e49306631e8b11fc92ecf94ccf69af1820b568
virtual <1185513330@qq.com>
2020-09-23 15:53:21 +0800

    Add spool test
    Signed-off-by: virtual <1185513330@qq.com>

---
+++ framework/test/SharpAbp.Abp.Spool.Tests/SharpAbp/Abp/Spool/AbpSpoolTestBase.cs
@@ -0,0 +1,13 @@
+﻿using Volo.Abp;
+using Volo.Abp.Testing;
+
+namespace SharpAbp.Abp.Spool
+{
+    public abstract class AbpSpoolTestBase : AbpIntegratedTest<AbpSpoolTestModule>
+    {
+        protected override void SetAbpApplicationCreationOptions(AbpApplicationCreationOptions options)
+        {
+            options.UseAutofac();
+        }
+    }
+}
