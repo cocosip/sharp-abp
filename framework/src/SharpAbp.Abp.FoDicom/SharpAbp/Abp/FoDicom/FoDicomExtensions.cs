d47842a0e8b26f87c048f54b8949464323f903b9
virtual <1185513330@qq.com>
2020-10-23 13:27:35 +0800

    Add ParseAsDicomDate,ParseAsDicomTime method
    Signed-off-by: virtual <1185513330@qq.com>

--- framework/src/SharpAbp.Abp.FoDicom/SharpAbp/Abp/FoDicom/FoDicomExtensions.cs
+++ framework/src/SharpAbp.Abp.FoDicom/SharpAbp/Abp/FoDicom/FoDicomExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using FellowOakDicom;
 using System;
 using System.Globalization;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace SharpAbp.Abp.FoDicom
@@ -8,7 +9,7 @@ namespace SharpAbp.Abp.FoDicom
     public static class FoDicomExtensions
     {
         /// <summary>
-        /// 获取日期类型时间
+        ///  Get Date from dicom format DicomDataset
         /// </summary>
         /// <param name="dataset"></param>
         /// <param name="tag"></param>
@@ -16,32 +17,45 @@ namespace SharpAbp.Abp.FoDicom
         /// <returns></returns>
         public static DateTime? GetDate(this DicomDataset dataset, DicomTag tag, DateTime? defaultValue = null)
         {
+            var dateValue = dataset.GetSingleValueOrDefault(tag, "");
+            return dateValue.ParseAsDicomDate(defaultValue);
+        }
+
+        /// <summary>
+        /// Parse dicom string format date as DateTime
+        /// </summary>
+        /// <param name="stringDateValue"></param>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
+        public static DateTime? ParseAsDicomDate(this string stringDateValue, DateTime? defaultValue = null)
+        {
             if (defaultValue == null)
             {
                 defaultValue = new DateTime(1970, 1, 1);
             }
-            var dateStringValue = dataset.GetSingleValueOrDefault(tag, "");
-            if (dateStringValue.IsNullOrWhiteSpace())
+
+            if (stringDateValue.IsNullOrWhiteSpace())
             {
                 return defaultValue;
             }
-            //将字符串转换成时间
-            if (Regex.IsMatch(dateStringValue, @"^[\d]{8}$"))
+
+            if (Regex.IsMatch(stringDateValue, @"^[\d]{8}$"))
             {
-                if (DateTime.TryParseExact(dateStringValue, "yyyyMMdd", CultureInfo.CurrentCulture, DateTimeStyles.None, out DateTime d1))
+                if (DateTime.TryParseExact(stringDateValue, "yyyyMMdd", CultureInfo.CurrentCulture, DateTimeStyles.None, out DateTime d1))
                 {
                     return d1;
                 }
             }
-            if (DateTime.TryParse(dateStringValue, out DateTime d2))
+            if (DateTime.TryParse(stringDateValue, out DateTime d2))
             {
                 return d2;
             }
             return defaultValue;
         }
 
+
         /// <summary>
-        /// 获取时间类型
+        /// Get DateTime from DicomDataset
         /// </summary>
         /// <param name="dataset"></param>
         /// <param name="tag"></param>
@@ -49,41 +63,53 @@ namespace SharpAbp.Abp.FoDicom
         /// <returns></returns>
         public static DateTime? GetTime(this DicomDataset dataset, DicomTag tag, DateTime? defaultValue = null)
         {
+            var stringTimeValue = dataset.GetSingleValueOrDefault(tag, "");
+            return stringTimeValue.ParseAsDicomTime(defaultValue);
+        }
+
+        /// <summary>
+        /// Parse dicom string format datetime as DateTime
+        /// </summary>
+        /// <param name="stringTimeValue"></param>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
+        public static DateTime? ParseAsDicomTime(this string stringTimeValue, DateTime? defaultValue = null)
+        {
             if (defaultValue == null)
             {
                 defaultValue = new DateTime(1970, 1, 1);
             }
-            var timeStringValue = dataset.GetSingleValueOrDefault(tag, "");
-            if (timeStringValue.IsNullOrWhiteSpace())
+
+            if (stringTimeValue.IsNullOrWhiteSpace())
             {
                 return defaultValue;
             }
 
-            if (Regex.IsMatch(timeStringValue, @"^[\d]{6}$"))
+            if (Regex.IsMatch(stringTimeValue, @"^[\d]{6}$"))
             {
-                if (DateTime.TryParseExact(timeStringValue, "HHmmss", CultureInfo.CurrentCulture, DateTimeStyles.None, out DateTime d1))
+                if (DateTime.TryParseExact(stringTimeValue, "HHmmss", CultureInfo.CurrentCulture, DateTimeStyles.None, out DateTime d1))
                 {
                     return new DateTime(1970, 1, 1, d1.Hour, d1.Minute, d1.Second);
                 }
             }
-            else if (Regex.IsMatch(timeStringValue, @"^[\d]{4}$"))
+            else if (Regex.IsMatch(stringTimeValue, @"^[\d]{4}$"))
             {
-                if (DateTime.TryParseExact(timeStringValue, "HHmm", CultureInfo.CurrentCulture, DateTimeStyles.None, out DateTime d1))
+                if (DateTime.TryParseExact(stringTimeValue, "HHmm", CultureInfo.CurrentCulture, DateTimeStyles.None, out DateTime d1))
                 {
                     return new DateTime(1970, 1, 1, d1.Hour, d1.Minute, d1.Second);
                 }
             }
-            else if (Regex.IsMatch(timeStringValue, @"^[\d]{6}\.[\d]{1,7}$"))
+            else if (Regex.IsMatch(stringTimeValue, @"^[\d]{6}\.[\d]{1,7}$"))
             {
 
-                var f = "HHmmss.".PadRight(timeStringValue.Length, 'f');
-                if (DateTime.TryParseExact(timeStringValue, f, CultureInfo.CurrentCulture, DateTimeStyles.None, out DateTime d1))
+                var f = "HHmmss.".PadRight(stringTimeValue.Length, 'f');
+                if (DateTime.TryParseExact(stringTimeValue, f, CultureInfo.CurrentCulture, DateTimeStyles.None, out DateTime d1))
                 {
                     return new DateTime(1970, 1, 1, d1.Hour, d1.Minute, d1.Second);
                 }
             }
 
-            if (DateTime.TryParse(timeStringValue, out DateTime d2))
+            if (DateTime.TryParse(stringTimeValue, out DateTime d2))
             {
                 return new DateTime(1970, 1, 1, d2.Hour, d2.Minute, d2.Second);
             }
@@ -92,7 +118,7 @@ namespace SharpAbp.Abp.FoDicom
 
 
         /// <summary>
-        /// 使用指定的编码格式对二进制数据进行解码
+        /// Use specified encode to encode value
         /// </summary>
         /// <param name="dataset"></param>
         /// <param name="tag"></param>
@@ -103,10 +129,23 @@ namespace SharpAbp.Abp.FoDicom
             if (dataset.Contains(tag))
             {
                 var buffer = dataset.GetValues<byte>(tag);
-                var value = DicomEncoding.GetEncoding(encode).GetString(buffer);
-                return value;
+                if (buffer.Any())
+                {
+                    return buffer.ParseAsDicomString(encode);
+                }
             }
-            return "";
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// Parse buffer to dicom encoding string
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <param name="encode"></param>
+        /// <returns></returns>
+        public static string ParseAsDicomString(this byte[] buffer, string encode = "GB18030")
+        {
+            return DicomEncoding.GetEncoding(encode).GetString(buffer);
         }
 
     }
