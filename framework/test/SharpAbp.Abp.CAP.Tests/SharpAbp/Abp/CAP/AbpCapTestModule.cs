c52d5da10894f8a4c3ebb574da4b0f8f6f52c4cb
virtual <1185513330@qq.com>
2020-11-04 16:05:49 +0800

    Add cap module and unit test,fix cap inject problem
    Signed-off-by: virtual <1185513330@qq.com>

---
+++ framework/test/SharpAbp.Abp.CAP.Tests/SharpAbp/Abp/CAP/AbpCapTestModule.cs
@@ -0,0 +1,37 @@
+﻿using DotNetCore.CAP;
+using Microsoft.Extensions.DependencyInjection;
+using Savorboard.CAP.InMemoryMessageQueue;
+using Volo.Abp;
+using Volo.Abp.Autofac;
+using Volo.Abp.Modularity;
+
+namespace SharpAbp.Abp.CAP
+{
+    [DependsOn(
+      typeof(AbpCapModule),
+      typeof(AbpTestBaseModule),
+      typeof(AbpAutofacModule)
+      )]
+    public class AbpCapTestModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            Configure<CapOptions>(options =>
+            {
+                options.UseInMemoryMessageQueue();
+                options.UseInMemoryStorage();
+
+                options.DefaultGroup = "test-group";
+                options.ConsumerThreadCount = 3;
+                options.FailedRetryCount = 10;
+                options.FailedRetryInterval = 30;
+                options.SucceedMessageExpiredAfter = 3600;
+                options.Version = "2.0";
+            });
+
+        }
+
+
+
+    }
+}
---
+++ framework/test/SharpAbp.Abp.CAP.Tests/SharpAbp/Abp/CAP/CapOptionsTest.cs
@@ -0,0 +1,29 @@
+﻿using DotNetCore.CAP;
+using Microsoft.Extensions.Options;
+using Xunit;
+
+namespace SharpAbp.Abp.CAP
+{
+    public class CapOptionsTest : AbpCapTestBase
+    {
+        private readonly CapOptions _options;
+
+        public CapOptionsTest()
+        {
+            _options = GetRequiredService<IOptions<CapOptions>>().Value;
+        }
+
+
+        [Fact]
+        public void Get_Configured_Option_Test()
+        {
+            Assert.Equal("test-group", _options.DefaultGroup);
+            Assert.Equal("2.0", _options.Version);
+            Assert.Equal(3, _options.ConsumerThreadCount);
+            Assert.Equal(10, _options.FailedRetryCount);
+            Assert.Equal(30, _options.FailedRetryInterval);
+            Assert.Equal(3600, _options.SucceedMessageExpiredAfter);
+        }
+
+    }
+}
