49aa5e957063ce02c5b38f81ef70469271a15a31
virtual <1185513330@qq.com>
2020-11-20 17:29:03 +0800

    Add Data Logging module
    Signed-off-by: virtual <1185513330@qq.com>

---
+++ framework/src/SharpAbp.Abp.Data.Logging/SharpAbp/Abp/Data/Logging/AbpDataLoggingModule.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.Modularity;
+
+namespace SharpAbp.Abp.Data.Logging
+{
+    public class AbpDataLoggingModule : AbpModule
+    {
+
+    }
+}
