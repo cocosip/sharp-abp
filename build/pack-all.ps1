4ecee9085c8c2d3d9085de0ae304614b16ce845e
virtual <1185513330@qq.com>
2021-01-07 20:06:06 +0800

    Fix pack bug. Signed-off-by: virtual <1185513330@qq.com>

--- build/pack-all.ps1
+++ build/pack-all.ps1
@@ -5,7 +5,7 @@
 foreach ($solutionPath in $solutionPaths) {    
     $solutionAbsPath = (Join-Path $rootFolder $solutionPath)
     Set-Location $solutionAbsPath
-    dotnet pack --configuration Release --no-build -o $rootPath\dest
+    dotnet pack -c:Release --include-symbols -o:$rootPath\dest
     if (-Not $?) {
         Write-Host ("Pack failed for the solution: " + $solutionPath)
         Set-Location $rootFolder
