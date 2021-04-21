0a689450fda5cc251260832c0d6db76506b9e653
virtual <1185513330@qq.com>
2021-04-21 10:50:31 +0800

    Add Dictionary Extensions
    Signed-off-by: virtual <1185513330@qq.com>

---
+++ framework/src/SharpAbp.Abp.Core/SharpAbp/Abp/Core/Extensions/AbpDictionaryExtensions.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SharpAbp.Abp.Core.Extensions
+{
+    public static class AbpDictionaryExtensions
+    {
+        public static bool TryAdd<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue value)
+        {
+            if (!dictionary.ContainsKey(key))
+            {
+                dictionary.Add(key, value);
+                return true;
+            }
+            return false;
+        }
+
+
+        public static TValue GetValueOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue defaultValue)
+        {
+            if (dictionary.TryGetValue(key, out TValue o))
+            {
+                return o;
+            }
+
+            return defaultValue;
+        }
+
+        public static bool Remove<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, out TValue value)
+        {
+            if (dictionary.TryGetValue(key, out value))
+            {
+                return dictionary.Remove(key);
+            }
+            return false;
+        }
+
+        public static IDictionary<TKey, TValue> ToDictionary<TKey, TValue>(this IEnumerable<KeyValuePair<TKey, TValue>> keyValuePairs)
+        {
+            var dictionary = new Dictionary<TKey, TValue>();
+            foreach (var keyValuePair in keyValuePairs)
+            {
+                if (!dictionary.ContainsKey(keyValuePair.Key))
+                {
+                    dictionary.Add(keyValuePair.Key, keyValuePair.Value);
+                }
+            }
+            return dictionary;
+        }
+
+        /// <summary>
+        /// Convert dictionary to new type dictionary
+        /// </summary>
+        /// <typeparam name="TKey">Source dictionary key type</typeparam>
+        /// <typeparam name="TValue">Source dictionary value type</typeparam>
+        /// <typeparam name="TTKey">Target dictionary key type</typeparam>
+        /// <typeparam name="TTValue">Target dictionary value type</typeparam>
+        /// <param name="dictionary"></param>
+        /// <param name="keySelector"></param>
+        /// <param name="valueSelector"></param>
+        /// <returns></returns>
+        public static IDictionary<TTKey, TTValue> ConvertDictionary<TKey, TValue, TTKey, TTValue>(
+            this IDictionary<TKey, TValue> dictionary,
+            Func<TKey, TTKey> keySelector,
+            Func<TValue, TTValue> valueSelector)
+        {
+            var dict = new Dictionary<TTKey, TTValue>();
+            foreach (var keyValuPair in dictionary)
+            {
+                dict.TryAdd(keySelector(keyValuPair.Key), valueSelector(keyValuPair.Value));
+            }
+            return dict;
+        }
+
+
+    }
+}
---
+++ framework/test/SharpAbp.Abp.Core.Tests/SharpAbp/Abp/Core/AbpDictionaryExtensionsTest.cs
@@ -0,0 +1,82 @@
+﻿using SharpAbp.Abp.Core.Extensions;
+using Xunit;
+
+namespace SharpAbp.Abp.Core
+{
+    public class AbpDictionaryExtensionsTest
+    {
+        [Fact]
+        public void TryAdd_Test()
+        {
+            var dict = new System.Collections.Generic.Dictionary<string, string>()
+            {
+                {"1","zhangsan" },
+                {"2","lisi" }
+            };
+
+            dict.TryAdd<string, string>("3", "wangwu");
+            Assert.Equal(3, dict.Count);
+            dict.TryAdd<string, string>("2", "lisi2");
+            Assert.Equal(3, dict.Count);
+            Assert.Equal("lisi", dict["2"]);
+        }
+
+        [Fact]
+        public void GetValueOrDefault_Test()
+        {
+            var dict = new System.Collections.Generic.Dictionary<string, string>()
+            {
+                {"123","xxx" },
+                {"456","yyy" },
+                {"789","" }
+            };
+
+            var v1 = dict.GetValueOrDefault("123", "33");
+            Assert.Equal("xxx", v1);
+
+            var v2 = dict.GetValueOrDefault("111", "xx");
+            Assert.Equal("xx", v2);
+
+            var v3 = dict.GetValueOrDefault("789", "x");
+            Assert.Equal("", v3);
+        }
+
+        [Fact]
+        public void Remove_Test()
+        {
+            var dict = new System.Collections.Generic.Dictionary<int, string>()
+            {
+                {1,"123" },
+                {2,"456" },
+                {3,"789" }
+            };
+
+            var r1 = dict.Remove(1, out string v1);
+            Assert.True(r1);
+            Assert.Equal("123", v1);
+
+            var r2 = dict.Remove(4, out string v2);
+            Assert.False(r2);
+            Assert.Null(v2);
+
+        }
+
+        [Fact]
+        public void ConvertDictionary_Test()
+        {
+            var dict = new System.Collections.Generic.Dictionary<int, string>()
+            {
+                {1,"11" },
+                {2,"22" },
+                {3,"33" }
+            };
+
+            var newDict = dict.ConvertDictionary<int, string, string, string>(x => x.ToString(), v => v);
+
+            Assert.Equal(3, newDict.Count);
+            Assert.True(newDict.ContainsKey("1"));
+            Assert.True(newDict.ContainsKey("2"));
+        }
+
+    }
+}
