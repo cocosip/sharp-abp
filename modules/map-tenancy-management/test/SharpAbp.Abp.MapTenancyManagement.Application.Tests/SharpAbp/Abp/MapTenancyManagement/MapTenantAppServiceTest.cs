478595087c0b053ada6a2781b7904ad700cb893f
virtual <1185513330@qq.com>
2021-04-25 11:13:02 +0800

    Fix mapt tenant bug, upgrade to v1.2.1
    Signed-off-by: virtual <1185513330@qq.com>

--- modules/map-tenancy-management/test/SharpAbp.Abp.MapTenancyManagement.Application.Tests/SharpAbp/Abp/MapTenancyManagement/MapTenantAppServiceTest.cs
+++ modules/map-tenancy-management/test/SharpAbp.Abp.MapTenancyManagement.Application.Tests/SharpAbp/Abp/MapTenancyManagement/MapTenantAppServiceTest.cs
@@ -24,9 +24,6 @@ namespace SharpAbp.Abp.MapTenancyManagement
             var tenant1 = await _tenantRepository.FindByNameAsync("tenant1");
             var tenant2 = await _tenantRepository.FindByNameAsync("tenant2");
 
-
-
-
             var id = await _mapTenantAppService.CreateAsync(new CreateMapTenantDto("100", tenant1.Id, "200"));
             var mapTenant1 = await _mapTenantAppService.GetAsync(id);
             var mapTenant2 = await _mapTenantAppService.GetByCodeAsync("100");
@@ -35,20 +32,25 @@ namespace SharpAbp.Abp.MapTenancyManagement
             Assert.Equal(mapTenant1.MapCode, mapTenant2.MapCode);
             Assert.Equal("200", mapTenant1.MapCode);
 
-            await _mapTenantAppService.UpdateAsync(new UpdateMapTenantDto(id, "300", tenant2.Id, "400"));
+            await _mapTenantAppService.UpdateAsync(new UpdateMapTenantDto(id, "300", tenant1.Id, "400"));
 
             var mapTenant3 = await _mapTenantAppService.GetByCodeAsync("300");
             Assert.Equal("300", mapTenant3.Code);
             Assert.Equal("400", mapTenant3.MapCode);
-            Assert.Equal(tenant2.Id, mapTenant3.TenantId);
+            Assert.Equal(tenant1.Id, mapTenant3.TenantId);
+
+            //Update code
+            await _mapTenantAppService.UpdateAsync(new UpdateMapTenantDto(id, "300", tenant2.Id, "400"));
+            var mapTenant4 = await _mapTenantAppService.GetAsync(id);
+
 
             var mapTenancyConfiguration = await _mapTenancyConfigurationProvider.GetAsync("300");
             Assert.Equal(mapTenancyConfiguration.TenantId, tenant2.Id);
 
             await _mapTenantAppService.DeleteAsync(id);
 
-            var mapTenant4 = await _mapTenantAppService.GetByCodeAsync("300");
-            Assert.Null(mapTenant4);
+            var mapTenant5 = await _mapTenantAppService.GetByCodeAsync("300");
+            Assert.Null(mapTenant5);
 
         }
     }
