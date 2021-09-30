ad28df079c357639ce20cfa52c5a927149f162ec
virtual <1185513330@qq.com>
2021-09-30 17:01:14 +0800

    Fix identity claimType controller missing method.
    Signed-off-by: virtual <1185513330@qq.com>

--- modules/Identity/src/SharpAbp.Abp.Identity.HttpApi/SharpAbp/Abp/Identity/IdentityClaimTypeController.cs
+++ modules/Identity/src/SharpAbp.Abp.Identity.HttpApi/SharpAbp/Abp/Identity/IdentityClaimTypeController.cs
@@ -26,9 +26,36 @@ namespace SharpAbp.Abp.Identity
         }
 
         [HttpGet]
+        [Route("any/{name}")]
+        public async Task<bool> AnyAsync(string name, Guid? ignoredId = null)
+        {
+            return await _identityClaimTypeAppService.AnyAsync(name, ignoredId);
+        }
+
+        [HttpGet]
         public async Task<PagedResultDto<IdentityClaimTypeDto>> GetPagedListAsync(IdentityClaimTypePagedRequestDto input)
         {
             return await _identityClaimTypeAppService.GetPagedListAsync(input);
+        }
+
+        [HttpPost]
+        public async Task<Guid> CreateAsync(CreateIdentityClaimTypeDto input)
+        {
+            return await _identityClaimTypeAppService.CreateAsync(input);
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public async Task UpdateAsync(Guid id, UpdateIdentityClaimTypeDto input)
+        {
+            await _identityClaimTypeAppService.UpdateAsync(id, input);
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task DeleteAsync(Guid id)
+        {
+            await _identityClaimTypeAppService.DeleteAsync(id);
         }
     }
 }
