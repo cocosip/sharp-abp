ddc760e6836b440508a32ff99d37133ac20db979
virtual <1185513330@qq.com>
2022-10-08 09:25:27 +0800

    Upgrade ABP to v6.0.0
    Signed-off-by: virtual <1185513330@qq.com>

--- modules/minid/app/MinIdApp.Domain/Users/AppUser.cs
+++ modules/minid/app/MinIdApp.Domain/Users/AppUser.cs
@@ -38,6 +38,8 @@ namespace MinIdApp.Users
 
         public virtual bool PhoneNumberConfirmed { get; private set; }
 
+        public virtual bool IsActive { get; private set; }
+
         #endregion
 
         /* Add your own properties here. Example:
@@ -57,7 +59,7 @@ namespace MinIdApp.Users
 
         private AppUser()
         {
-            
+
         }
     }
 }
