bb84285ebd70b95b2157bb062df5c1862b2d835d
virtual <1185513330@qq.com>
2023-02-15 16:33:23 +0800

    Fix dotnet sdk build failed bug
    Signed-off-by: virtual <1185513330@qq.com>

--- build/pack-all.ps1
+++ build/pack-all.ps1
@@ -5,7 +5,7 @@
 foreach ($solutionPath in $solutionPaths) {    
     $solutionAbsPath = (Join-Path $rootFolder $solutionPath)
     Set-Location $solutionAbsPath
-    dotnet pack -c:Release --include-symbols -o:$rootPath\dest
+    dotnet pack -c:Release --include-symbols --no-build -o:$rootPath\dest
     if (-Not $?) {
         Write-Host ("Pack failed for the solution: " + $solutionPath)
         Set-Location $rootFolder
