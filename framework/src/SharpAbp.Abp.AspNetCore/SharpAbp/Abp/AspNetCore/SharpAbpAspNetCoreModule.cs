1e4aa471eab267ee21b1fddfede645bde2b25dcb
cocosip <1185513330@qq.com>
2024-02-05 10:52:33 +0800

    add missing code
    Signed-off-by: cocosip <1185513330@qq.com>

--- framework/src/SharpAbp.Abp.AspNetCore/SharpAbp/Abp/AspNetCore/SharpAbpAspNetCoreModule.cs
+++ framework/src/SharpAbp.Abp.AspNetCore/SharpAbp/Abp/AspNetCore/SharpAbpAspNetCoreModule.cs
@@ -1,5 +1,10 @@
-﻿using Volo.Abp.AspNetCore;
+﻿using SharpAbp.Abp.AspNetCore.FrontHost;
+using SharpAbp.Abp.AspNetCore.Http;
+using SharpAbp.Abp.AspNetCore.Response;
+using System.Threading.Tasks;
+using Volo.Abp.AspNetCore;
 using Volo.Abp.Modularity;
+using Volo.Abp.Threading;
 
 namespace SharpAbp.Abp.AspNetCore
 {
@@ -8,6 +13,20 @@ namespace SharpAbp.Abp.AspNetCore
         )]
     public class SharpAbpAspNetCoreModule : AbpModule
     {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            AsyncHelper.RunSync(() => ConfigureServicesAsync(context));
+        }
 
+        public override Task ConfigureServicesAsync(ServiceConfigurationContext context)
+        {
+            Configure<AbpHttpResponseHeaderOptions>(options => { });
+            Configure<AbpFrontHostOptions>(options => { });
+            Configure<AbpHttpHeadersOptions>(options =>
+            {
+                options.RouteTranslationPrefix = "X-Abp";
+            });
+            return Task.CompletedTask;
+        }
     }
 }
