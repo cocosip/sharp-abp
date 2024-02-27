9a813aeb08f68e06701d6a25eb892520ea86d7b5
cocosip <1185513330@qq.com>
2024-02-27 16:55:36 +0800

    adjust version
    Signed-off-by: cocosip <1185513330@qq.com>

--- framework/src/SharpAbp.Abp.TransformSecurity/SharpAbp/Abp/TransformSecurity/AbpTransformSecurityModule.cs
+++ framework/src/SharpAbp.Abp.TransformSecurity/SharpAbp/Abp/TransformSecurity/AbpTransformSecurityModule.cs
@@ -1,6 +1,10 @@
 ﻿using SharpAbp.Abp.Crypto;
+using SharpAbp.Abp.Crypto.RSA;
+using SharpAbp.Abp.Crypto.SM2;
+using System.Threading.Tasks;
 using Volo.Abp.Modularity;
 using Volo.Abp.ObjectExtending;
+using Volo.Abp.Threading;
 
 namespace SharpAbp.Abp.TransformSecurity
 {
@@ -10,6 +14,33 @@ namespace SharpAbp.Abp.TransformSecurity
         )]
     public class AbpTransformSecurityModule : AbpModule
     {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            AsyncHelper.RunSync(() => ConfigureServicesAsync(context));
+        }
+
+        public override Task ConfigureServicesAsync(ServiceConfigurationContext context)
+        {
+            Configure<AbpTransformSecurityRSAOptions>(options =>
+            {
+                options.Padding = RSAPaddingNames.PKCS1Padding;
+                options.KeySize = 2048;
+            });
+
+            Configure<AbpTransformSecuritySM2Options>(options =>
+            {
+                options.Curve = Sm2EncryptionNames.CurveSm2p256v1;
+                options.Mode = Org.BouncyCastle.Crypto.Engines.SM2Engine.Mode.C1C2C3;
+            });
+
+            Configure<AbpTransformSecurityOptions>(options =>
+            {
+                options.Enabled = false;
+                options.EncryptionAlgo = "RSA";
+            });
+
+            return Task.CompletedTask;
+        }
 
     }
 }
