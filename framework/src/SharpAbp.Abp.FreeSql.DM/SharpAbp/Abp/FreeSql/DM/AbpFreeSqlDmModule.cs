240dfaadf443bdf3a605da4b7df9c1e7457ab975
cocosip <1185513330@qq.com>
2024-09-30 16:30:30 +0800

    Add freeSql dameng
    Signed-off-by: cocosip <1185513330@qq.com>

---
+++ framework/src/SharpAbp.Abp.FreeSql.DM/SharpAbp/Abp/FreeSql/DM/AbpFreeSqlDmModule.cs
@@ -0,0 +1,11 @@
+﻿using Volo.Abp.Modularity;
+
+namespace SharpAbp.Abp.FreeSql.DM
+{
+    [DependsOn(
+        typeof(AbpFreeSqlModule)
+        )]
+    public class AbpFreeSqlDmModule : AbpModule
+    {
+    }
+}
