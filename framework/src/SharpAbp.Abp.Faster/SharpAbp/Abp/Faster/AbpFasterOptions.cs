055dcd2e98e611cf059810ad5aa8be379cd0d615
virtual <1185513330@qq.com>
2025-01-13 16:55:05 +0800

    faster log configure from file
    Signed-off-by: virtual <1185513330@qq.com>

--- framework/src/SharpAbp.Abp.Faster/SharpAbp/Abp/Faster/AbpFasterOptions.cs
+++ framework/src/SharpAbp.Abp.Faster/SharpAbp/Abp/Faster/AbpFasterOptions.cs
@@ -1,4 +1,7 @@
-﻿namespace SharpAbp.Abp.Faster
+﻿using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+
+namespace SharpAbp.Abp.Faster
 {
     public class AbpFasterOptions
     {
@@ -10,6 +13,42 @@
         public AbpFasterOptions()
         {
             Configurations = new AbpFasterConfigurations();
+        }
+
+        public AbpFasterOptions Configure(IConfiguration configuration)
+        {
+            var fasterConfigurations = configuration
+                .GetSection("FasterOptions:Configurations")
+                .Get<Dictionary<string, AbpFasterConfiguration>>();
+
+            RootPath = configuration["FasterOptions:RootPath"];
+
+            if (fasterConfigurations != null)
+            {
+                foreach (var kv in fasterConfigurations)
+                {
+                    Configurations.Configure(kv.Key, c =>
+                    {
+                        var val = kv.Value;
+                        c.FileName = val.FileName;
+                        c.PreallocateFile = val.PreallocateFile;
+                        c.Capacity = val.Capacity;
+                        c.RecoverDevice = val.RecoverDevice;
+                        c.UseIoCompletionPort = val.UseIoCompletionPort;
+                        c.DisableFileBuffering = val.DisableFileBuffering;
+                        c.ScanUncommitted = val.ScanUncommitted;
+                        c.AutoRefreshSafeTailAddress = val.AutoRefreshSafeTailAddress;
+                        c.CommitIntervalMillis = val.CommitIntervalMillis;
+                        c.CompleteIntervalMillis = val.CompleteIntervalMillis;
+                        c.TruncateIntervalMillis = val.TruncateIntervalMillis;
+                        c.PreReadCapacity = val.PreReadCapacity;
+                        c.AutoRetiredMillis = val.AutoRetiredMillis;
+                        c.IteratorName = val.IteratorName;
+                    });
+                }
+            }
+
+            return this;
         }
     }
 }
