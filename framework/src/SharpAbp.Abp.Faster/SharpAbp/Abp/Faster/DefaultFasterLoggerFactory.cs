633aeee29e0a52196b88cfb5c05d80dcb57f8b27
virtual <1185513330@qq.com>
2025-01-24 16:30:50 +0800

    fix bug
    Signed-off-by: virtual <1185513330@qq.com>

--- framework/src/SharpAbp.Abp.Faster/SharpAbp/Abp/Faster/DefaultFasterLoggerFactory.cs
+++ framework/src/SharpAbp.Abp.Faster/SharpAbp/Abp/Faster/DefaultFasterLoggerFactory.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using Volo.Abp.DependencyInjection;
 
@@ -9,8 +10,7 @@ namespace SharpAbp.Abp.Faster
 {
     public class DefaultFasterLoggerFactory : IFasterLoggerFactory, ISingletonDependency
     {
-        private readonly object _lock = new();
-        protected Dictionary<string, IFasterLogger> _loggers;
+        protected ConcurrentDictionary<string, IFasterLogger> _loggers;
         protected AbpFasterOptions Options { get; }
         protected ILogger Logger { get; }
         protected IServiceProvider ServiceProvider { get; }
@@ -29,26 +29,17 @@ namespace SharpAbp.Abp.Faster
 
         public virtual IFasterLogger<T> GetOrCreate<T>(string name) where T : class
         {
-
-            lock (_lock)
+            var configuration = Options.Configurations.GetConfiguration(name);
+            var logger = _loggers.GetOrAdd(name, () =>
             {
-                var configuration = Options.Configurations.GetConfiguration(name);
-                if (_loggers.TryGetValue(name, out IFasterLogger logger))
-                {
-                    return logger as IFasterLogger<T>;
-                }
-
                 var fasterLogger = ActivatorUtilities.CreateInstance<FasterLogger<T>>(
-                      ServiceProvider,
-                      name,
-                      configuration);
-                fasterLogger.Initialize();
-                if (!_loggers.TryAdd(name, fasterLogger))
-                {
-                    Logger.LogDebug("GetOrCreate faster logger add failed. {name}", name);
-                }
+                   ServiceProvider,
+                   name,
+                   configuration);
                 return fasterLogger;
-            }
+            }).As<IFasterLogger<T>>();
+
+            return logger;
         }
 
     }
