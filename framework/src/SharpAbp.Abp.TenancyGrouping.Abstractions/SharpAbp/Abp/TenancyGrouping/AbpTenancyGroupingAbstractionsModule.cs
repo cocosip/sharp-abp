f04019f17f968299b7357aec2c1f17d6e20e4fed
virtual <1185513330@qq.com>
2025-04-16 15:24:37 +0800

    add tenancy grouping resources
    Signed-off-by: virtual <1185513330@qq.com>

--- framework/src/SharpAbp.Abp.TenancyGrouping.Abstractions/SharpAbp/Abp/TenancyGrouping/AbpTenancyGroupingAbstractionsModule.cs
+++ framework/src/SharpAbp.Abp.TenancyGrouping.Abstractions/SharpAbp/Abp/TenancyGrouping/AbpTenancyGroupingAbstractionsModule.cs
@@ -1,7 +1,12 @@
 ﻿using System.Threading.Tasks;
+using SharpAbp.Abp.TenancyGrouping.Localization;
+using Volo.Abp.Localization;
+using Volo.Abp.Localization.ExceptionHandling;
 using Volo.Abp.Modularity;
 using Volo.Abp.MultiTenancy;
+using Volo.Abp.MultiTenancy.Localization;
 using Volo.Abp.Threading;
+using Volo.Abp.VirtualFileSystem;
 
 namespace SharpAbp.Abp.TenancyGrouping
 {
@@ -17,17 +22,22 @@ namespace SharpAbp.Abp.TenancyGrouping
 
         public override Task ConfigureServicesAsync(ServiceConfigurationContext context)
         {
-            //Configure<AbpVirtualFileSystemOptions>(options =>
-            //{
-            //    options.FileSets.AddEmbedded<AbpMultiTenancyAbstractionsModule>();
-            //});
+            Configure<AbpVirtualFileSystemOptions>(options =>
+            {
+                options.FileSets.AddEmbedded<AbpTenancyGroupingAbstractionsModule>();
+            });
 
-            //Configure<AbpLocalizationOptions>(options =>
-            //{
-            //    options.Resources
-            //        .Add<AbpMultiTenancyResource>("en")
-            //        .AddVirtualJson("/Volo/Abp/MultiTenancy/Localization");
-            //});
+            Configure<AbpLocalizationOptions>(options =>
+            {
+                options.Resources
+                    .Add<AbpMultiTenancyResource>("en")
+                    .AddVirtualJson("/SharpAbp/Abp/TenancyGrouping/Localization/Resources");
+            });
+
+            Configure<AbpExceptionLocalizationOptions>(options =>
+            {
+                options.MapCodeNamespace("AbpTenancyGrouping", typeof(AbpTenancyGroupingResource));
+            });
 
             return Task.CompletedTask;
         }
